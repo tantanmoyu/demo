2e2ee985fb2c9bec0d8e14bbf892a7347afb8120
tantanmoyu <406388456@qq.com>
2019-07-02 16:27:04 +0800

    1

--- demo01-01/src/main/java/servlet/MyServlet.java
+++ demo01-01/src/main/java/servlet/MyServlet.java
@@ -16,7 +16,7 @@ public class MyServlet implements Servlet {
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        //niubi
+        //niub
         res.getWriter().write("hehe");
     }
 
